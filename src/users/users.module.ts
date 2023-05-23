6e755e7464fd8ea76868b91063104cfacc1814ab
VTellesRg <vini.m.telles@gmail.com>
2023-05-23 15:21:52 -0300

    add: users, posts, comments schemas modules

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,13 @@
+
 import { Module } from '@nestjs/common';
-import { UsersService } from './users.service';
+import { MongooseModule } from '@nestjs/mongoose';
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { User, UserSchema } from './entities/user.entity';
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: User.name, schema: UserSchema }])],
   controllers: [UsersController],
-  providers: [UsersService]
+  providers: [UsersService],
 })
 export class UsersModule {}
