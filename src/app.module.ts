e59a6577d24f18af77453a3eb623bfce08560f2c
VTellesRg <vini.m.telles@gmail.com>
2023-05-23 11:46:48 -0300

    add: first steps backend api

---
+++ src/app.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { PostsModule } from './posts/posts.module';
+import { CommentsModule } from './comments/comments.module';
+import { MongooseModule } from '@nestjs/mongoose/dist/mongoose.module';
+
+@Module({
+  imports: [MongooseModule.forRoot("mongodb+srv://VTelles:<password>@cluster0.b2lfwdb.mongodb.net/?retryWrites=true&w=majority"), UsersModule, PostsModule, CommentsModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
