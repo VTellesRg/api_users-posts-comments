de0a14b697fff05b1104e72b08d9b9e71c150a19
VTellesRg <vini.m.telles@gmail.com>
2023-05-23 14:48:18 -0300

    add: read&write mongodb acess

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,7 +7,7 @@ import { CommentsModule } from './comments/comments.module';
 import { MongooseModule } from '@nestjs/mongoose/dist/mongoose.module';
 
 @Module({
-  imports: [MongooseModule.forRoot("mongodb+srv://VTelles:<password>@cluster0.b2lfwdb.mongodb.net/?retryWrites=true&w=majority"), UsersModule, PostsModule, CommentsModule],
+  imports: [MongooseModule.forRoot("mongodb+srv://usuariox:GTuJf2pOvuHNPRzc@cluster0.b2lfwdb.mongodb.net/?retryWrites=true&w=majority"), UsersModule, PostsModule, CommentsModule],
   controllers: [AppController],
   providers: [AppService],
 })
