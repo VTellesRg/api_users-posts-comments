7ea80115c7db4e49b72d8cc86e0f0eafd23ed7b4
VTellesRg <vini.m.telles@gmail.com>
2023-05-24 14:32:42 -0300

    add: user service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,32 +1,41 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { User } from './entities/user.entity';
+import { User, UserDocument } from './entities/user.entity';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 
 @Injectable()
 export class UsersService {
 
-  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
 
   create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+    const user = new this.userModel(createUserDto);
+    return user.save();
+    // return 'This action adds a new user';
   }
 
   findAll() {
-    return `This action returns all users`;
+    return this.userModel.find();
+    // return `This action returns all users`;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  findOne(id: string) {
+    return this.userModel.findById(id);
+    // return `This action returns a #${id} user`;
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  update(id: string, updateUserDto: UpdateUserDto) {
+    return this.userModel.findByIdAndUpdate(
+      {_id: id,},
+      { updateUserDto,},
+      { new: true,})
+    // return `This action updates a #${id} user`;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  remove(id: string) {
+    return this.userModel.deleteOne({_id: id,}).exec();
+    // return `This action removes a #${id} user`;
   }
 }
